0eedfeda3ac217941df3022cdeee34cd9a6e22aa
Fukuchi Kentaro <ken3ken@me.com>
2020-06-26 22:09:02 +0900

    yarnでライブラリをインストールした

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const axios = require('axios');
+// require関数でモジュールとして取得。
+axios.get('https://www.google.com').then(res => {
+    //axiosのgetメソッドにリクエストのURLとそのレスポンスを受け取った際の処理を無名関数で渡している
+    console.log(res.data);
+});
